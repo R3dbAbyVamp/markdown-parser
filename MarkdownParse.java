44b43dc1f09413e701266a96d690fb1adcc4e441
R3dbAbyVamp <ppeng@ucsd.edu>
2022-05-04 16:36:03 -0700

    Trying to fix errors bc of import and arg[0] thing

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -67,7 +67,8 @@ public class MarkdownParse {
 
             // Check if there's a new line after the close parentheses. 
             // If yes, skip the search below for the actual close parentheses
-            if (closeParen != (markdown.length() - 1) && !(markdown.substring(closeParen + 1, closeParen + 2)).equals("\n")) {
+            if (closeParen != (markdown.length() - 1) && 
+            !(markdown.substring(closeParen + 1, closeParen + 2)).equals("\n")) {
                 int closeParenFirst = closeParen; 
                 closeParen = markdown.indexOf(")", closeParenFirst + 1);
                 System.out.println("closeParen Actual: " +closeParen);
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;    // Wild card import of assert methods for junit? 
 import org.junit.*;       // Wild card import for junit?
+import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,6 +19,34 @@ public class MarkdownParseTest {       // Class declaration
     // -------- Test getLinks() ----------//
     @Test 
     public void getLinksTest() throws IOException {
+        // Path and filename to get string for file name
+        Path fileName = Path.of("/Users/paul/Documents/Github/markdown-parser/test-file.md");
+        String content = Files.readString(fileName);
+
+        // // Try-catch block for IOException
+        // try { 
+        //     content = Files.readString(fileName);
+        // } 
+        // catch (IOException e) {
+        //     System.out.println("IOException caught");
+        // } 
+
+        // What we expect
+        ArrayList<String> expectedOutput = new ArrayList<>();
+        expectedOutput.add("https://something.com"); 
+        expectedOutput.add("some-thing.html");
+
+        // Actual
+        ArrayList<String> answer = MarkdownParse.getLinks(content);
+        // assertArrayEquals(expectedOutput, answer);
+        assertEquals(expectedOutput, answer);
+
+    }
+
+
+
+    @Test 
+    public void getLinksTest2() throws IOException {
         // Path and filename to get string for file name
         Path fileName = Path.of("/Users/paul/Documents/Github/markdown-parser/test-file.md");
         String content = Files.readString(fileName);
